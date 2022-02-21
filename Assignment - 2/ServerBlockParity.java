413a12f11a1eec6383d53783a092048e27f0f405
Hardik Togadiya <hardiktogadiya123@gmail.com>
2022-02-21 21:35:03 +0530

    Update Assignment-2

---
+++ Assignment - 2/ServerBlockParity.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class ServerBlockParity{
+	public static void main(String args[])throws Exception
+	{
+		try
+		{
+			Scanner sc=new Scanner(System.in);
+			ServerSocket serversocket = new ServerSocket(6001);
+			System.out.println("Server Is Listening On Localhost : 6001");
+			
+			Socket socket = serversocket.accept();
+			DataInputStream istream = new DataInputStream(socket.getInputStream());
+			DataOutputStream ostream = new DataOutputStream(socket.getOutputStream());
+			
+			String rmsg = new String();
+			String omsg  = new String();
+			rmsg=istream.readUTF();
+			char cpmsg[][] = new char[rmsg.length()/8][8];
+			int rowerror =0 , colerror = 0;
+			
+			
+			System.out.println("Received Binary String    : "+rmsg);
+			
+				
+			for(int i=0;i<rmsg.length();i+=8)
+			{
+				String rtemp = rmsg.substring(i,i+8);
+				char p = evenParity(rtemp);
+				
+				
+				if(p == '0')
+				{
+					System.out.println("Row : " + i/8 + " No Error");
+				}
+				else
+				{
+					rowerror++;
+					System.out.println("Row : " + i/8 + " Error Detected");
+				}
+				
+				cpmsg[i/8] = rtemp.toCharArray();
+				rtemp = rtemp.substring(0,rtemp.length()-1);
+				omsg = omsg + (char)Byte.parseByte(rtemp,2);
+			}
+			for(int j=0;j<8;j++)
+			{
+				String ctemp = new String();
+				for(int i=0;i<rmsg.length()/8;i++)
+				{
+					ctemp = ctemp + cpmsg[i][j];
+				}
+				char p = evenParity(ctemp);
+				
+				
+				
+				if(p == '0')
+				{
+					System.out.println("Column : " + j + " No Error");
+				}
+				else
+				{
+					colerror++;
+					System.out.println("Column : " + j + " Error Detected");
+				}
+			}
+			if(rowerror == 0 && colerror == 0)
+			{
+				omsg = omsg.substring(0,omsg.length()-1);
+				System.out.println("\nOriginal Message : "+omsg);
+			}
+		
+			
+			
+			
+			istream.close();
+			socket.close();
+			serversocket.close();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	public static char evenParity(String msg)
+	{
+		int c=0;
+		
+		for(int j=0;j<msg.length();j++)
+		{
+			if(msg.charAt(j)=='1')
+			{
+				c++;
+			}
+		}
+		//System.out.println("Counter : "+c);
+		if(c%2==0)
+		{
+			return '0';
+		}
+		else
+		{
+			return '1';
+		}
+	}
+
+}
---
+++ Assignment - 2/p1_server-1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.*;
+class p1_server
+{
+	public static void main(String args[])throws Exception
+	{
+		try
+		{
+			ServerSocket serversocket = new ServerSocket(6363);
+			Socket socket=serversocket.accept();
+			DataInputStream instream=new DataInputStream(socket.getInputStream());
+			DataOutputStream outstream=new DataOutputStream(socket.getOutputStream());
+			
+			String finalmsg=instream.readUTF();
+			int i,j,k,counter=0;
+			String f="",op="";
+			
+			for(i=0;i<finalmsg.length();i+=8)
+			{
+				String decode=finalmsg.substring(i,i+8);
+				System.out.println("Message Checking:"+decode);
+				for(j=0;j<decode.length();j++)
+				{
+					if(decode.charAt(j)=='1')
+						counter++;
+				}
+				if(counter%2==0)
+					System.out.println("Correct message passes of character : "+((i/8)+1));
+				else
+				{
+					System.out.println("Please send the message again..");
+					System.exit(0);
+				}
+			}
+			System.out.println("Message received successfully...");
+			for(k=0;k<finalmsg.length();k+=8)
+			{
+				op=finalmsg.substring(k,k+7);
+				System.out.println("After removing parity bit:"+op);
+				f+=(char)Byte.parseByte(op,2);
+			}
+			System.out.println("Final Message:"+f);
+			
+			instream.close();
+			socket.close();
+			serversocket.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
