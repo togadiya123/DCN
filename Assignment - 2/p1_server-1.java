1bdab19a8b4e1a25b85e004a6d96010ccd9ae712
Hardik Togadiya <hardiktogadiya123@gmail.com>
2022-04-30 16:45:09 +0530

    Add Some other practice program

--- Assignment - 2/p1_server-1.java
+++ Assignment - 2/p1_server-1.java
@@ -1,53 +1,85 @@
-import java.io.*;
 import java.net.*;
-class p1_server
-{
-	public static void main(String args[])throws Exception
-	{
-		try
-		{
-			ServerSocket serversocket = new ServerSocket(6363);
-			Socket socket=serversocket.accept();
-			DataInputStream instream=new DataInputStream(socket.getInputStream());
-			DataOutputStream outstream=new DataOutputStream(socket.getOutputStream());
-			
-			String finalmsg=instream.readUTF();
-			int i,j,k,counter=0;
-			String f="",op="";
-			
-			for(i=0;i<finalmsg.length();i+=8)
-			{
-				String decode=finalmsg.substring(i,i+8);
-				System.out.println("Message Checking:"+decode);
-				for(j=0;j<decode.length();j++)
-				{
-					if(decode.charAt(j)=='1')
-						counter++;
-				}
-				if(counter%2==0)
-					System.out.println("Correct message passes of character : "+((i/8)+1));
-				else
-				{
-					System.out.println("Please send the message again..");
-					System.exit(0);
-				}
-			}
-			System.out.println("Message received successfully...");
-			for(k=0;k<finalmsg.length();k+=8)
-			{
-				op=finalmsg.substring(k,k+7);
-				System.out.println("After removing parity bit:"+op);
-				f+=(char)Byte.parseByte(op,2);
-			}
-			System.out.println("Final Message:"+f);
-			
-			instream.close();
-			socket.close();
-			serversocket.close();
-		}
-		catch(Exception e)
-		{
-			System.out.println(e);
-		}
-	}
+import java.io.*;
+import java.util.Scanner;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+class p6_client {
+
+    public static void main(String args[]) throws Exception {
+        try {
+            DatagramSocket ds1 = new DatagramSocket();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter msg: ");
+            String msg = sc.nextLine();
+
+            String kstring = "sahildcnsahildcn";
+            SecretKey key = new SecretKeySpec(kstring.getBytes(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            String cmsg = new String(cipher.doFinal(msg.getBytes()));
+            System.out.println("\nCipher msg = " + cmsg);
+
+
+            InetAddress ip = InetAddress.getByName("localhost");
+            DatagramPacket dp1 = new
+                    DatagramPacket(cmsg.getBytes(), cmsg.length(), ip, 6363);
+            ds1.send(dp1);
+
+            DatagramSocket ds2 = new DatagramSocket(6565);
+            byte[] buf = new byte[500];
+            DatagramPacket dp2 = new DatagramPacket(buf, 500);
+            ds2.receive(dp2);
+
+            String msg1 = new String(buf);
+            3111 - Kumbhani Sanket
+
+            System.out.println("server:" + msg1);
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+		import java.net.*;import java.io.*;
+                import java.util.Scanner;import java.security.*;
+                import javax.crypto.*;import javax.crypto.spec.*;
+
+class p6_server {
+    public static void main(String args[]) throws Exception {
+
+        try {
+            DatagramSocket ds1 = new DatagramSocket(6363);
+            byte[] buf = new byte[500];
+            DatagramPacket dp1 = new DatagramPacket(buf, 500);
+            ds1.receive(dp1);
+            ds1.close();
+
+            String cmsg = new String(dp1.getData(), 0, dp1.getLength());
+            System.out.println("Received msg = " + cmsg);
+            DatagramSocket ds2 = new DatagramSocket();
+            String kstring = "sahildcnsahildcn";
+            SecretKey key = new SecretKeySpec(kstring.getBytes(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+
+            String dcmsg = new String(cipher.doFinal(cmsg.getBytes()));
+            System.out.println("\nDe-Cipher msg = " + dcmsg);
+
+
+            InetAddress ip = InetAddress.getByName("localhost");
+            DatagramPacket dp2 = new
+                    DatagramPacket(dcmsg.getBytes(), dcmsg.length(), ip, 6565);
+            ds2.send(dp2);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
